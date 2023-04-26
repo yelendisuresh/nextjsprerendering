094b24735de705e2ad43160dba8eb8e030f5d9f0
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-26 12:53:29 +0530

    getStaticPath -possible values for dynamicId(postId)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 function Home() {
   return (
     <div>
       <h1>Nextjs pre rendering</h1>
+      <Link href="/users">users</Link>
+      <Link href="/posts">posts</Link>
     </div>
   );
 }
---
+++ pages/posts/index.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+function PostList({ posts }) {
+  return (
+    <>
+      <h1>List Of Posts</h1>
+      {posts.map(post => {
+        return (
+          <div key={post.id}>
+            <Link href={`posts/${post.id}`} passHref>
+              <h2>
+                {post.id}
+                {post.title}
+              </h2>
+            </Link>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+export default PostList;
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
+}
