92a79a9ba5ef2898ecc5d0f71c0d689a06686fd5
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-27 11:32:20 +0530

    SSR with Dynamic Parameters

---
+++ pages/news/[category].js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const ArticleListByCategory = ({ articles, category }) => {
+  return (
+    <>
+      <h1> Showing news for {category}</h1>
+      {articles.map(article => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+            <p> {article.description}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ArticleListByCategory;
+
+export async function getServerSideProps(context) {
+  const {
+    params: { category },
+  } = context;
+  //   const { category } = { params };
+  console.log("category", context);
+  const response = await fetch(
+    `http://localhost:4000/news?category=${category}`
+  );
+  const data = await response.json();
+  console.log("data", data);
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
+}
