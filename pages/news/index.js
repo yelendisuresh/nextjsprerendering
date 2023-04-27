438502cccf2e61f36c2fb920a27fb55373ef92c7
ysureshbabu <Yelendi.SureshBabu@ghd.com>
2023-04-27 11:03:21 +0530

    SSR with getServerSideProps

---
+++ pages/news/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const NewsArticleList = ({ articles }) => {
+  return (
+    <div>
+      <h1>List Of news Articles</h1>
+      {articles.map(article => {
+        return (
+          <div key={article.id}>
+            {" "}
+            <h2>
+              {article.id} {article.title} {article.category}
+            </h2>{" "}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewsArticleList;
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:4000/news");
+  const data = await response.json();
+
+  return {
+    props: {
+      articles: data,
+    },
+  };
+}
